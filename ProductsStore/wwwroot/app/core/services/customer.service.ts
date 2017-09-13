a7177a6dea13c25442be40cbf550b84d25fe4ed6
Carlos Anaya <carlos.anaya@oliverwyman.com>
2017-09-13 15:13:18 -0500

    Implement Angular Services for getting customers and states

---
+++ ProductsStore/wwwroot/app/core/services/customer.service.ts
@@ -0,0 +1,60 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+import { ICustomer } from '../../shared/interfaces';
+
+
+@Injectable()
+export class CustomerService {
+
+    baseUrl: string = '/api/customers';
+
+    constructor(private http: Http) { }
+
+    getCustomers(): Observable<ICustomer[]> {
+        return this.http.get(this.baseUrl)
+            .map((res: Response) => {
+                let customers = res.json();
+                this.calculateCustomersOrderTotal(customers);
+                return customers;
+            })
+            .catch(this.handleError);
+    }
+
+    getCustomer(id: string): Observable<ICustomer[]> {
+        return this.http.get(this.baseUrl + '/' + id)
+            .map((res: Response) => res.json())
+            .catch(this.handleError);
+    }
+
+    calculateCustomersOrderTotal(customers: ICustomer[]) {
+        for (let customer of customers) {
+            if (customer && customer.orders) {
+                let total = 0;
+                for (let order of customer.orders) {
+                    total += (order.price * order.quantity);
+                }
+                customer.orderTotal = total;
+            }
+        }
+    }
+
+    private handleError(error: any) {
+        console.error('server error', error);
+        if (error instanceof Response) {
+            let errorMessage = '';
+            try {
+                errorMessage = error.json().error;
+            } catch (e) {
+                errorMessage = error.statusText;
+            }
+            return Observable.throw(errorMessage);
+        }
+        return Observable.throw(error || '.NET Core server error');
+    }
+}
---
+++ ProductsStore/wwwroot/app/core/services/state.service.ts
@@ -0,0 +1,37 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+import { IState } from '../../shared/interfaces';
+
+@Injectable()
+export class StateService {
+
+    baseUrl: string = '/api/states';
+
+    constructor(private http: Http) { }
+
+    getStates(): Observable<IState> {
+        return this.http.get(this.baseUrl)
+            .map((res: Response) => res.json())
+            .catch(this.handleError);
+    }
+
+    private handleError(error: any) {
+        console.error('server error', error);
+        if (error instanceof Response) {
+            let errorMessage = '';
+            try {
+                errorMessage = error.json().error;
+            } catch (e) {
+                errorMessage = error.statusText;
+            }
+            return Observable.throw(errorMessage);
+        }
+        return Observable.throw(error || '.NET Core server error');
+    }
+}
