0ba6e543d60e295afa91ac3fd803244d93f21b66
Carlos Anaya <carlos.anaya@oliverwyman.com>
2017-09-14 12:05:42 -0500

    Implement Capitalize and Trim Pipes

---
+++ ProductsStore/wwwroot/app/shared/pipes/trim.pipe.ts
@@ -0,0 +1,10 @@
+﻿import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'trim' })
+export class TrimPipe implements PipeTransform {
+    transform(value: any) {
+        if (!value)
+            return '';
+        return value.trim();
+    }
+}
--- ProductsStore/wwwroot/app/shared/shared.module.ts
+++ ProductsStore/wwwroot/app/shared/shared.module.ts
@@ -2,8 +2,12 @@
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { CapitalizePipe } from './pipes/capitalize.pipe';
+import { TrimPipe } from './pipes/trim.pipe';
+
 @NgModule({
     imports: [CommonModule, FormsModule, ReactiveFormsModule],
-    exports: [CommonModule, FormsModule, ReactiveFormsModule]
+    declarations: [CapitalizePipe, TrimPipe],
+    exports: [CommonModule, FormsModule, ReactiveFormsModule, CapitalizePipe, TrimPipe]
 })
 export class SharedModule { }
